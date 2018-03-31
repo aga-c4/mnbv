8248ae381cefed3f1c1304f916309770a4ca4bdc
aga-c4 <aga-c4@rambler.ru>
2018-03-31 13:01:29 +0300

    robots

--- modules/robots/controller/RobotsStartcronController.class.php
+++ modules/robots/controller/RobotsStartcronController.class.php
@@ -47,7 +47,7 @@ class RobotsStartcronController extends AbstractMnbvsiteController{
         require_once MNBV_PATH . MOD_MODELSPATH . 'MNBVProcess.class.php';  //Класс работы со словарями
         require_once MNBV_PATH . MOD_MODELSPATH . 'MNBVRobot.class.php';  //Класс работы со словарями
         $proc = new MNBVRobot($procId);
-        $procProp = $proc->getObj();
+        $procProp = getObj();
 
         if ($procProp!==null && (empty($procProp['sid']) || $procProp['sid']==$rsid)) {//Продолжаем работу только если данное задание не имеет sid, т.е. не запущено.
 
@@ -89,7 +89,7 @@ class RobotsStartcronController extends AbstractMnbvsiteController{
                 if (!isset($task['vars']['crmonth']) || !MNBVf::validateValByStr($crmonth,$task['vars']['crmonth'])) continue;
                 if (!isset($task['vars']['crweek']) || !MNBVf::validateValByStr($crweek,$task['vars']['crweek'])) continue;
 
-                $stProc = new MNBVRobot(Glob::$vars['robotsRunStorage'],$task["id"]);
+                $stProc = new MNBVRobot($task["id"]);
                 if ($stProc->start((!empty($task['vars']['always']))?'restart':'')) echo date("Y-m-d G:i:s") . ' ' . $stProc->getRobotAlias() . " sid=[" . $stProc->getPsid() . "]\n";
             }
 
