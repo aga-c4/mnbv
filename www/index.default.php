907b431ff9e3123767c89465b9d95c65e27ce2d3
aga-c4 <aga-c4@rambler.ru>
2018-03-31 18:31:42 +0300

    logs

--- www/index.default.php
+++ www/index.default.php
@@ -3,7 +3,7 @@
 //define("APP_DEBUG_MODE",true); //Режим отладки true - активирован, false - не используется, если не задать, то используются системные настройки
 $startFromConsole = false; //Маркер запуска из консоли
 $startTplMode = 'html'; //Формат ответа (txt,html,json,none)
-$systemLogView = true; //Видимость Лога работы скрипта
+$systemLogView = false; //Видимость Лога работы скрипта
 $systemLogRTView = false; //Выводить сообщения непосредственно при их формировании
 $systemLogSave = false; //Сохранение Лога работы скрипта
 $systemErrorsView = false; //Видимость Ошибок работы скрипта
