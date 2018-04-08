1383a80f42c2d003a99c614c07321b7f40ccf330
aga-c4 <aga-c4@rambler.ru>
2018-04-08 17:19:19 +0300

    lb add

--- templates/bsdefsite/units/wdg_imagesblock.php
+++ templates/bsdefsite/units/wdg_imagesblock.php
@@ -18,11 +18,20 @@ if (isset($item['sub_obj']['files']["img"]["1"]) && $tecObjTxtCode = MNBVf::getO
 } else {//Никакого специфического объекта не нашли, выводим изображения по стандартной схеме
     if (isset($item['sub_obj']['files']['img']["1"])){
         //Подготовим размеры изображений
-        $imgWStr = (isset($item['sub_obj']['files']["img"]["1"]['size']['w']))?(' width="'.$item['sub_obj']['files']["img"]["1"]['size']['w'].'"'):'';
-        if ($imgWStr=='' && isset($item['img_max_size']['img_max_w'])) $imgWStr = ' width="'. $item['img_max_size']['img_max_w'] .'"';
-        $imgHStr = (isset($item['sub_obj']['files']["img"]["1"]['size']['h']))?(' height="'.$item['sub_obj']['files']["img"]["1"]['size']['h'].'"'):'';
+        if (empty($item['sub_obj']['files']["img"]["1"]["url"])){
+            $imgWStr = (isset($item['sub_obj']['files']["img"]["1"]['size']['w']))?(' width="'.$item['sub_obj']['files']["img"]["1"]['size']['w'].'"'):'';
+            if ($imgWStr=='' && isset($item['img_max_size']['img_max_w'])) $imgWStr = ' width="'. $item['img_max_size']['img_max_w'] .'"';
+            $imgHStr = (isset($item['sub_obj']['files']["img"]["1"]['size']['h']))?(' height="'.$item['sub_obj']['files']["img"]["1"]['size']['h'].'"'):'';
+        }else{
+            $imgWStr = $imgHStr = '';
+        }
+        // data-gallery="gallery1" чтоб привеязать к одной галерее с последовательным переходом.
         ?>
-        <a class="example-image-link" href='<?=SysBF::getFrArr($item['sub_obj']['files']["img"]["1"],'src_big','');?>' data-toggle="lightbox" data-gallery="<?=SysBF::getFrArr($item['sub_obj']['files']["img"]["1"],'name','');?>"><img class="example-image" src='<?=SysBF::getFrArr($item['sub_obj']['files']["img"]["1"],'src','');?>'<?=$imgWStr;?><?=$imgWStr;?> style="max-width: 98%;"></a><br>
+        <a href='<?=SysBF::getFrArr($item['sub_obj']['files']["img"]["1"],'src_big','');?>' 
+           data-toggle="lightbox" data-title="<?=SysBF::getFrArr($item['sub_obj']['files']["img"]["1"],'name','');?>">
+            <img src='<?=SysBF::getFrArr($item['sub_obj']['files']["img"]["1"],'src','');?>'<?=$imgWStr;?><?=$imgWStr;?> 
+                style="max-width: 98%;">
+        </a><br>
     <?
     }
 }
@@ -43,7 +52,7 @@ for ($i=2;$i<=$formImgNum;$i++){
         $u = SysBF::getFrArr($item['sub_obj']['files']["img"]["$i"],'src_min','');
 
         //URL ссылка большой картинки
-        $a1 = (SysBF::getFrArr($item['sub_obj']['files']["img"]["$i"],'src_big'))?'<a href="'.$item['sub_obj']['files']["img"]["$i"]['src_big'].'" data-toggle="lightbox" data-gallery="'.$n.'">':'';
+        $a1 = (SysBF::getFrArr($item['sub_obj']['files']["img"]["$i"],'src_big'))?'<a href="'.$item['sub_obj']['files']["img"]["$i"]['src_big'].'" data-toggle="lightbox" data-title="'.$n.'">':'';
         $a2 = (!empty($a1))?'</a>':'';
 
         echo $a1 . '<img src="' . $u . '""' . $imgWStr . $imgWStr . '>' . $a2 . "\n";
