7f3641fe046c4c0534f07fa651f7f8bad79af63d
aga-c4 <aga-c4@rambler.ru>
2018-04-20 13:25:07 +0300

    proc

--- modules/robots/controller/RobotsRobotsmonitorController.class.php
+++ modules/robots/controller/RobotsRobotsmonitorController.class.php
@@ -118,9 +118,9 @@ class RobotsRobotsmonitorController extends AbstractMnbvsiteController{
                     );
 
                     if (!empty($procProp['action']['command'])) {
-                        $commandName = (!empty($commandNameArr[$procProp['action']['command']]))?$commandNameArr[$procProp['action']['command']]:'';
+                        $commandName = (!empty($commandNameArr[$procProp['action']['command']]))?$commandNameArr[$procProp['action']['command']]:'Bad command';
                         $commandName = trim(strtolower($commandName));
-                        SysBF::saveFile($outputFilename3,date("Y-m-d H:i:s") . "\nGet command: ".$commandNameArr[$commandName]."\n",'a');
+                        SysBF::saveFile($outputFilename3,"\n" . date("Y-m-d H:i:s") . "Get command: " . $commandNameArr[$commandName] . "\n",'a');
                         if (isset($commandNameArr[$procProp['action']['command']])) {
 
                             if ($commandName=='stop_all'){
