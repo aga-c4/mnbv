ea8253888f26e08a43eb5265749918a04792f6ca
aga-c4 <aga-c4@rambler.ru>
2018-12-31 03:21:20 +0300

    url

--- modules/mnbv/model/MNBVURL.class.php
+++ modules/mnbv/model/MNBVURL.class.php
@@ -68,7 +68,7 @@ class MNBVURL {
         $catalias = (isset($stRes[1])&&isset($stRes[1]['catalias']))?$stRes[1]['catalias']:'';
 
         $result = '/';
-        if (!empty($this->urlTypes[$urltype]['mod_pref'])) $result .= $this->urlTypes[$urltype]['mod_pref'].'/';
+        if (!empty($this->urlTypes[$urltype]['mod_pref'])) $result .= $this->urlTypes[$urltype]['mod_pref'];
         if ($objtype==1){//Папка
             $result .= $alias;
         }else{//Объект
