3cbe92155471257b9122395b4a32af8bf8b80256
aga-c4 <aga-c4@rambler.ru>
2019-01-02 17:44:19 +0300

    url

--- modules/mnbvsite/controller/PglistController.class.php
+++ modules/mnbvsite/controller/PglistController.class.php
@@ -152,14 +152,17 @@ class PglistController extends AbstractMnbvsiteController {
         if (!empty($item['obj']['use_other_storage'])) {
             //Папка
             SysLogs::addLog('parent=[' . $realFolder['parent']['id'] . '] <=> folder_start_id=[' . $item['obj']['folder_start_id'] . ']');
-            if (!empty($realFolder['parent']['id']) && $realFolder['parent']['id']!=$item['obj']['folder_start_id']) {
-                $item['obj']['up_folder_url'] = MNBVf::generateObjUrl($realFolder['parent'],array('altlang'=>!Lang::isDefLang(),'type'=>'list'));
-                $currName = MNBVf::getItemName($realFolder['parent'],!Lang::isDefLang());
-                $item['obj']['nav_arr'][4] = array('name'=>$currName,'url'=>$item['obj']['up_folder_url']); //Текущая папка
+            
+            if (!empty($realFolder['id']) && $realFolder['id']!=$item['obj']['folder_start_id']) {
+                if (!empty($realFolder['parent']['id']) && $realFolder['parent']['id']!=$item['obj']['folder_start_id']) {
+                    $item['obj']['up_folder_url'] = MNBVf::generateObjUrl($realFolder['parent'],array('altlang'=>!Lang::isDefLang(),'type'=>'list'));
+                    $currName = MNBVf::getItemName($realFolder['parent'],!Lang::isDefLang());
+                    $item['obj']['nav_arr'][4] = array('name'=>$currName,'url'=>$item['obj']['up_folder_url']); //Текущая папка
+                }
+                //Текущий объект
+                $currName = MNBVf::getItemName($realFolder,!Lang::isDefLang());
+                $item['obj']['nav_arr'][5] = array('name'=>$currName,'url'=>$item['page_url']);
             }
-            //Текущий объект
-            $currName = MNBVf::getItemName($realFolder,!Lang::isDefLang());
-            $item['obj']['nav_arr'][5] = array('name'=>$currName,'url'=>$item['page_url']);
         }
         //Конец обработки хлебных крошек ---------------------------------------
         
