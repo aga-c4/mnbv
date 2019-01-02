a853e74a34698f86a3bcdd6cf1d203bf75bb93ee
aga-c4 <aga-c4@rambler.ru>
2019-01-02 23:54:08 +0300

    url

--- modules/mnbvsite/controller/PglistController.class.php
+++ modules/mnbvsite/controller/PglistController.class.php
@@ -62,7 +62,7 @@ class PglistController extends AbstractMnbvsiteController {
                 $item['obj']['folder_alias'] = (!empty($realFolder['alias']))?$realFolder['alias']:'';
             }
         }
-        $item['page_list_url'] = MNBVf::generateObjUrl($realFolder,array('altlang'=>Lang::isAltLang(),'type'=>'list'));
+        $item['page_list_url'] = MNBVf::generateObjUrl($realFolder['parent'],array('altlang'=>Lang::isAltLang(),'type'=>'list'));
         
         SysLogs::addLog('page_list_url: [' . $item['page_list_url'] . ']');        
         SysLogs::addLog('Select mnbv script storage: [' . $storage . '] page_main_alias=['.$item['obj']['page_main_alias'].']');
