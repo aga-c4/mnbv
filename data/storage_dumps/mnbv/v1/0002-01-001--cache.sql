e29dcc7a724a5a1f90f437df1034a876684df23f
Konstantin G Khachaturyan <aga-c4@rambler.ru>
2019-02-12 15:17:43 +0300

    Cache Db add

---
+++ data/storage_dumps/mnbv/v1/0002-01-001--cache.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 27 2018 г., 18:39
+-- Версия сервера: 5.6.38
+-- Версия PHP: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `j2i`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `mnbv_urlaliases`
+--
+
+DROP TABLE IF EXISTS `mnbv_tmp`;
+CREATE TABLE `mnbv_urlaliases` (
+  `id` varchar(100) NOT NULL DEFAULT '',
+  `ts` int(11) NOT NULL DEFAULT '0',
+  `tsto` int(11) NOT NULL DEFAULT '0',
+  `val` int(2) text NOT NULL
+) ENGINE=MEMORY DEFAULT CHARSET=utf8;
+
+--
+-- Индексы таблицы `mnbv_urlaliases`
+--
+ALTER TABLE `mnbv_urlaliases`
+  ADD PRIMARY KEY (`id`);
+  
+ COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
