5299f04a95816b5b92c17618e44786ed82a8422c
Konstantin Khachaturyan <aga-c4@rambler.ru>
2021-08-21 23:40:59 +0300

    bann

--- templates/bsdefsite2/units/wdg_actionslist.php
+++ templates/bsdefsite2/units/wdg_actionslist.php
@@ -6,17 +6,18 @@
 $carouselCaptions = array();
 $carouselItems = array();
 $itemCounter=0;
+$ban_img_id = SysBF::getFrArr($item,'ban_img_id',1);
 foreach ($item['list'] as $key=>$value) if ($key>0) {
-    if (!isset($value['files']["img"]["1"]) || $tecObjTxtCode = MNBVf::getObjCodeByURL(SysBF::getFrArr($value['files']["img"]["1"],'src',''))) continue;
+    if (!isset($value['files']["img"]["$ban_img_id"]) || $tecObjTxtCode = MNBVf::getObjCodeByURL(SysBF::getFrArr($value['files']["img"]["$ban_img_id"],'src',''))) continue;
     $carouselCaptions[] = '<li data-target="#carouselCaptions" data-slide-to="'.$itemCounter.'"'.((!$itemCounter)?' class="active"':'').'"></li>';
     
     $carouselItem = '<div class="carousel-item'.((!$itemCounter)?' active':'').'">'."\n";
     //Подготовим размеры изображений
-    $imgWStr = (isset($value['files']["img"]["1"]['size']['w']))?(' width="'.$value['files']["img"]["1"]['size']['w'].'"'):'';
+    $imgWStr = (isset($value['files']["img"]["$ban_img_id"]['size']['w']))?(' width="'.$value['files']["img"]["$ban_img_id"]['size']['w'].'"'):'';
     if ($imgWStr=='' && isset($item['img_max_size']['img_min_max_w'])) $imgWStr = ' width="'. $item['img_max_size']['img_min_max_w'] .'"';
-    $imgHStr = (isset($value['files']["img"]["1"]['size']['h']))?(' height="'.$value['files']["img"]["1"]['size']['h'].'"'):'';
-    //$carouselItem .= '<a href="' . SysBF::getFrArr($value,'url','') . '"><img src="' . SysBF::getFrArr($value['files']["img"]["1"],'src_min','') . '" class="d-block w-100" alt="' . MNBVf::getItemName($value,Lang::isAltLang()) . '"></a>'."\n";
-    $carouselItem .= '<a href="' . SysBF::getFrArr($value,'url','') . '"><img src="' . SysBF::getFrArr($value['files']["img"]["1"],'src','') . '" class="d-block w-100" alt="' . MNBVf::getItemName($value,Lang::isAltLang()) . '">'."\n";
+    $imgHStr = (isset($value['files']["img"]["$ban_img_id"]['size']['h']))?(' height="'.$value['files']["img"]["$ban_img_id"]['size']['h'].'"'):'';
+    //$carouselItem .= '<a href="' . SysBF::getFrArr($value,'url','') . '"><img src="' . SysBF::getFrArr($value['files']["img"]["$ban_img_id"],'src_min','') . '" class="d-block w-100" alt="' . MNBVf::getItemName($value,Lang::isAltLang()) . '"></a>'."\n";
+    $carouselItem .= '<a href="' . SysBF::getFrArr($value,'url','') . '"><img src="' . SysBF::getFrArr($value['files']["img"]["$ban_img_id"],'src','') . '" class="d-block w-100" alt="' . MNBVf::getItemName($value,Lang::isAltLang()) . '">'."\n";
     //Заголовок и краткое описание
     /* Если нужно выводить заголовок акции с описанием поверх баннера - раскомментить
     $carouselItem .= '<div class="carousel-caption d-sm-block">'."\n";
