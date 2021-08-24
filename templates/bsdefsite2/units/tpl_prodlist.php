6b8d2b3eb09106d066c13aa2316bf82905e6072e
Konstantin Khachaturyan <aga-c4@rambler.ru>
2021-08-24 12:22:59 +0300

    prods

--- templates/bsdefsite2/units/tpl_prodlist.php
+++ templates/bsdefsite2/units/tpl_prodlist.php
@@ -64,7 +64,7 @@ function CheckListSort(){
 <? } ?>
 </div>
 
-<div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-4 g-1">
+<div class="row row-cols-<?=(count($item['list'])>1)?'2':'1';?> row-cols-md-3 row-cols-lg-4 row-cols-xl-4 g-1">
 <?
 //Список объектов
 $folderCounter = 0;
